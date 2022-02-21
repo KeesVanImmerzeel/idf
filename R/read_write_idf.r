93699c6cadce3bfdeacb9684ad8dcb3508d536d0
Immerzeel <kees.vanimmerzeel@sweco.nl>
2022-02-21 17:40:36 +0100

    terra used instead of raster

--- R/read_write_idf.r
+++ R/read_write_idf.r
@@ -1,10 +1,9 @@
-# To do 2022-2-20:
+# To do 2022-2-21:
 # 1. add reading/writing double precision idf
-# 2. move to terra package (instead of raster)
 
 # ----------------------------------------------------------------------------
-#' Methods to create a RasterLayer object with the ability to read idf files.
-#'
+#' Methods to create a terra::SpatRaster object with the ability to read idf files.
+
 #' The function 'read_raster' is able to create a RasterLayer object from an
 #' idf-file if an idf-filename is specified (exctension '.idf', like in 'foo.idf').
 #' Otherwhise the function equals the function 'raster::raster()'.
@@ -12,16 +11,11 @@
 #' i.e. library(raster)
 #'
 #' @param x (character) filename
-#' @param crs (CRS) Coordinate Reference System. Optional.
-#'    If an idf-file is specified and this argument is missing,
-#'    the sp::CRS("+init=epsg:28992") is used.
-#'    For other raster filetypes: the CRS specified is used;
-#'       if it is not specified, the CRS is read from file (if existant).
-#' @param ... Additional arguments as for 'raster' function.
-#'    For an idf-raster, only the optional 'crs' argument is used.
-#' @return RasterLayer
-#'    N.B.: For your own files, omit the 'system.file' and 'package="raster"' bits
-#'    these are just to get the path to files installed with the package.
+#' @param EPSG Four-five digit number which represents a particular CRS definition.
+#'    If an idf-file is specified and this argument is missing, EPSG=28992 is used.
+#' @param ... Additional arguments as for 'terra::rast' function.
+#'    For an idf-raster, only the optional 'EPSG' argument is used.
+#' @return terra::SpatRaster
 #' @examples
 #'
 #' f <- system.file("extdata", "test.idf", package="idf")
@@ -33,17 +27,17 @@
 #' r <- read_raster(f)
 #'
 #' @export
-read_raster <- function(x, crs=NULL, ...) {
+read_raster <- function(x, EPSG=NULL, ...) {
    if ((typeof(x) == "character") &
        (.is_idf_extension(fileutils::get_filename_extension(x)))) {
-      if (missing(crs)) {
-         crs <- sp::CRS("+init=epsg:28992")
+      if (missing(EPSG)) {
+         EPSG=28992
       }
-      reslt <- .read.idf(x, crs)
+      reslt <- .read.idf(x, EPSG)
    } else {
-      reslt <- suppressWarnings(raster::raster(x, ...))
-      if (!missing(crs)) {  # Force crs if explicitly specified
-         raster::crs(reslt) <- sp::CRS("+init=epsg:28992")
+      reslt <- suppressWarnings(terra::rast(x, ...))
+      if (!missing(EPSG)) {  # Force crs if explicitly specified
+         terra::crs(reslt) <- 28992
       }
    }
    return(reslt)
@@ -53,37 +47,31 @@ read_raster <- function(x, crs=NULL, ...) {
 
 #' Write raster data to a file.
 #'
-#' \code{write_raster} writes an entire Raster* object to a file, using one of
-#' the many supported formats or the \code{\link[raster]{raster-package}} or the
-#' idf file format as used in the imod modeling software
+#' \code{write_raster} writes an entire terra::SpatRaster object to a file, using one of
+#' the many supported formats in the 'terra' package or an idf file format as used in the imod modeling software
 #' (\url{https://oss.deltares.nl/web/imod}).
 #'
 #' To use the 'idf' output format, don't specify the 'format argument' but use
-#' the'.idf' extension in the filename. Only RasterLayer objects can be written
-#' to an idf-file (no RasterStack or RasterBrick). Of the additional arguments
-#' of \code{\link[raster]{writeRaster}}, only the 'overwrite' may be used in
+#' the'.idf' extension in the filename. Only terra::SpatRaster objects with depth=0 can be written
+#' to an idf-file. Of the additional arguments
+#' of \code{\link[terra]{writeRaster}}, only the 'overwrite' may be used in
 #' case the idf-format is specified. In that case, all other additional
 #' arguments of the function \code{write_raster} are ignored.
-#' @param x RasterLayer Raster* object
+#' @param x terra::SpatRaster object
 #' @param filename Output filename
-#' @param format Character. Output file type. See
-#'   \code{\link[raster]{writeFormats}}. If this argument is not provided, it is
-#'   attempted to infer it from the filename extension. If that fails, the
-#'   default format is used. The default format is 'raster', but this can be
-#'   changed using \code{\link[raster]{rasterOptions}}.
+#' @param filetype Character. File format expresses as GDAL driver names.
+#'   If this argument is not provided, it is attempted to infer it from the filename extension.
 #' @param e Extent object
 #' @param constant multiplication factor (numeric)
-#' @param ... Additional arguments as in \code{\link[raster]{writeRaster}}.
+#' @param ... Additional arguments as in 'terra::writeRaster'.
 #' @return This function is used for the side-effect of writing values to a file.
 #' @importFrom magrittr %<>%
 #' @export
-write_raster <- function(x, filename, format=NULL, e=NULL, constant=NULL, ...) {
+write_raster <- function(x, filename, filetype=NULL, e=NULL, constant=NULL, ...) {
    # Write raster layer to idf file.
-   #
-
-   # @param x RasterLayer Raster* object
-   # @param filename Output (idf) filename
-   # @param overwrite (boolean) if true overwrite possible existing idf file.
+   # @param x  terra::SpatRaster object
+   # @param filename Output filename
+   # @param overwrite (boolean) if true overwrite possible existing file.
    # @examples
    #
    # f <- system.file("extdata", "test.tif", package="idf")
@@ -97,26 +85,28 @@ write_raster <- function(x, filename, format=NULL, e=NULL, constant=NULL, ...) {
       if ((overwrite == TRUE) |
           (overwrite == FALSE & (!file.exists(filename)))) {
          con = file(filename, "wb")
-         ncols <- x@ncols
-         nrows <- x@nrows
-         xll <- x@extent[1]
-         xur <- x@extent[2]
-         xlr <- x@extent[3]
-         yur <- x@extent[4]
+         ncols <- terra::ncol(x)
+         nrows <- terra::nrow(x)
+         xll <- terra::xmin(x)
+         xur <- terra::xmax(x)
+         xlr <- terra::ymin(x)
+         yur <- terra::ymax(x)
+         mindata <- terra::minmax(x)[1]
+         maxdata <- terra::minmax(x)[2]
+         NAflg <- terra::NAflag(x)
          writeBin(as.integer(c(1271, ncols, nrows)),
                   con,
                   size = 4,
                   endian = "little")
-
          writeBin(
             c(
                xll,
                xur,
                xlr,
                yur,
-               x@data@min,
-               x@data@max,
-               x@file@nodatavalue
+               mindata,
+               maxdata,
+               NAflg
             ),
             con,
             size = 4,
@@ -129,7 +119,7 @@ write_raster <- function(x, filename, format=NULL, e=NULL, constant=NULL, ...) {
          dx <- (xur - xll) / ncols
          dy <- (yur - xlr) / nrows
          writeBin(c(dx, dy), con, size = 4, endian = "little")
-         writeBin(x[], con, size = 4, endian = "little")
+         writeBin(as.vector(terra::values(x)), con, size = 4, endian = "little")
          close(con)
       } else {
          stop("Error in .write.idf: file exists; use overwrite=TRUE")
@@ -137,20 +127,23 @@ write_raster <- function(x, filename, format=NULL, e=NULL, constant=NULL, ...) {
    }
 
    if (!is.null(e)) {
-      x %<>% raster::crop(e)
+      x %<>% terra::crop(e)
    }
-   if (!is.null(constant) & constant != 1) {
-      x[] <- x[] * constant
+   if (!is.null(constant)) {
+      terra::values(x) <- terra::values(x) #* constant
    }
-   if ((is.null(format)) &
+   if ((is.null(filetype)) &
        (.is_idf_extension(fileutils::get_filename_extension(filename))) &
-       class(x) == "RasterLayer") {
+       class(x) == "SpatRaster") {
+      print(".write.idf")
       .write.idf(x, filename, ...)
    } else {
-      if (!is.null(format)) {
-         suppressWarnings(raster::writeRaster(x, filename, prj=TRUE, ...))
+      if (is.null(filetype)) {
+         print("write using file extension to determine file type")
+         suppressWarnings(terra::writeRaster(x, filename, ...))
       } else {
-         suppressWarnings(raster::writeRaster(x, filename, format, prj=TRUE, ...))
+         print("write using specified file extension ")
+         suppressWarnings(terra::writeRaster(x, filename, filetype, ...))
       }
    }
 }
@@ -171,95 +164,83 @@ write_raster <- function(x, filename, format=NULL, e=NULL, constant=NULL, ...) {
    toupper(ext) == ".IDF"
 }
 
-# ----------------------------------------------------------------------------
-
-# All possible output types of the 'raster::writeRaster' function.
-# @return (character) output types that can be specified for the 'raster::writeRaster' function.
-.writeFormats <- function() {
-   x <- raster::writeFormats()
-   return(x[,1])
-}
 
 # ----------------------------------------------------------------------------
 # Read raster layer from idf file.
 #
 # @param x Name of idf-file (*.idf)
-# @param crs PROJ.4 string to set the CRS. Default is epsg projection 28992 - amersfoort.
-# @return RasterLayer
-#    N.B.: For your own files, omit the 'system.file' and 'package="raster"' bits
-#    these are just to get the path to files installed with the package.
+# @param crs EPSG Four-five digit number which represents a particular CRS definition.
+# @return terra::SpatRaster object
 # @examples
 #
 # f <- system.file("extdata", "test.idf", package="idf")
 # f
 # r <- .read.idf(f)
 #
-.read.idf <- function(x, crs=sp::CRS("+init=epsg:28992")) {
-      con = file(x, "rb")
-      vars = readBin(con,
-                     integer(),
-                     n = 3,
-                     size = 4,
-                     endian = "little")
-      lrl_id = vars[1] # Lahey Record Length Identification; 1271 is a single precision IDF, 2296 a double precision.
-      ncol = vars[2]
-      nrow = vars[3]
-      vars = readBin(con,
-                     double(),
-                     n = 7,
-                     size = 4,
-                     endian = "little")
-      xll = vars[1]
-      xur = vars[2]
-      yll = vars[3]
-      yur = vars[4]
-      minval = vars[5]
-      maxval = vars[6]
-      nodata = vars[7]
-      vars = readBin(con,
-                     logical(),
-                     n = 4,
-                     size = 1,
-                     endian = "little")
-      ieq = vars[1]
-      itb = vars[2]
-      if (ieq |
-          itb)
-            stop("Non-equidistant IDF's or IDF's with tops and bottoms not supported yet...")
-      vars = readBin(con,
-                     double(),
-                     n = 2,
-                     size = 4,
-                     endian = "little")
-      dx = vars[1]
-      dy = vars[2]
-      data = readBin(con,
-                     double(),
-                     n = ncol * nrow,
-                     size = 4,
-                     endian = "little")
-      data = matrix(data,
-                    nrow = nrow,
-                    ncol = ncol,
-                    byrow = TRUE)
-      close(con)
+.read.idf <- function(x, EPSG=28992) {
+   con = file(x, "rb")
+   vars = readBin(con,
+                  integer(),
+                  n = 3,
+                  size = 4,
+                  endian = "little")
+   ncol = vars[2]
+   nrow = vars[3]
+   vars = readBin(con,
+                  double(),
+                  n = 7,
+                  size = 4,
+                  endian = "little")
+   xll = vars[1]
+   xur = vars[2]
+   yll = vars[3]
+   yur = vars[4]
+   minval = vars[5]
+   maxval = vars[6]
+   nodata = vars[7]
+   vars = readBin(con,
+                  logical(),
+                  n = 4,
+                  size = 1,
+                  endian = "little")
+   ieq = vars[1]
+   itb = vars[2]
+   if (ieq |
+       itb)
+      stop("Non-equidistant IDF's or IDF's with tops and bottoms not supported yet...")
+   vars = readBin(con,
+                  double(),
+                  n = 2,
+                  size = 4,
+                  endian = "little")
+   dx = vars[1]
+   dy = vars[2]
+   data = readBin(con,
+                  double(),
+                  n = ncol * nrow,
+                  size = 4,
+                  endian = "little")
+   data = matrix(data,
+                 nrow = nrow,
+                 ncol = ncol,
+                 byrow = TRUE)
+   close(con)
 
-      layer <-
-            raster::raster(
-                  nrows = nrow,
-                  ncols = ncol,
-                  xmn = xll,
-                  xmx = xur,
-                  ymn = yll,
-                  ymx = yur,
-                  crs = crs
-            )
-      raster::NAvalue(layer) <- nodata
-      layer[] <- data
+   layer <-
+      terra::rast(
+         nrows = nrow,
+         ncols = ncol,
+         xmin = xll,
+         xmax = xur,
+         ymin = yll,
+         ymax = yur,
+         vals=data
+      )
+   terra::NAflag(layer)<-nodata
 
-      names(layer) <- fileutils::bare_filename(x)
+   names(layer) <- fileutils::bare_filename(x)
 
-      return(layer)
+   return(layer)
 }
 
 # ----------------------------------------------------------------------------
